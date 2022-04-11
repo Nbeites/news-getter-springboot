311a232cbec33979329fb305ccdf051f28d0275f
nbeites <nm.beites@gmail.com>
2022-04-11 12:06:36 +0100

    minor

--- commands.sh
+++ commands.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Execute Tests
+mvn test
+
+
 #Execute Spring application
 CMD="java -Djava.security.egd=file:/dev/./urandom -jar news-0.0.1-SNAPSHOT.jar"
 $CMD &
@@ -7,4 +11,6 @@ SERVICE_PID=$!
 
 #Wait for Spring execution
 wait "$SERVICE_PID"
+
+
 
